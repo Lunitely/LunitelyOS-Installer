63be332af8b19e0d95d35e20648876f3610a5363
Elibrio <unconfigured@null.spigotmc.org>
2024-04-28 21:52:33 +0200

    ef ejoifuehfp9o8if

--- InstallWindow.xaml.cs
+++ InstallWindow.xaml.cs
@@ -1,22 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
+﻿using System.Windows;
 
 namespace LunitelyOS
 {
-    /// <summary>
-    /// Interaktionslogik für InstallWindow.xaml
-    /// </summary>
     public partial class InstallWindow : Window
     {
         public InstallWindow()
@@ -24,5 +9,6 @@ namespace LunitelyOS
             InitializeComponent();
             DarkMode.EnableDarkMode(this);
         }
+
     }
 }
