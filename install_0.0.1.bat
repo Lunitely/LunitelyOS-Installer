e246c414727aae07a529a8caddba237eec581db3
FerrousInk <139216162+FerrousInk@users.noreply.github.com>
2024-05-02 22:32:06 +0200

    Update install_0.0.1.bat

--- install_0.0.1.bat
+++ install_0.0.1.bat
@@ -1,5 +1,8 @@
 @echo off
 echo Installing 0.0.1 (Debug message)
-reg   
+reg delete "HKLM\SYSTEM\Setup" /v CmdLine /f
+reg add "HKLM\SYSTEM\Setup" /v CmdLine /t REG_SZ /d ""
+reg delete "HKLM\SYSTEM\Setup" /v SetupType /f
+reg add "HKLM\SYSTEM\Setup" /v SetupType /t REG_DWORD /d 0
 taskkill /f /im LunitelyOOBE.exe
 pause
