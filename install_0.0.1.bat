cf891478fde74464821d4bd12183bc421899b4aa
FerrousInk <139216162+FerrousInk@users.noreply.github.com>
2024-05-02 22:32:31 +0200

    Update install_0.0.1.bat

--- install_0.0.1.bat
+++ install_0.0.1.bat
@@ -1,5 +1,9 @@
 @echo off
 echo Installing 0.0.1 (Debug message)
+rem Commands Here
+
+goto finish
+:finish
 reg delete "HKLM\SYSTEM\Setup" /v CmdLine /f
 reg add "HKLM\SYSTEM\Setup" /v CmdLine /t REG_SZ /d ""
 reg delete "HKLM\SYSTEM\Setup" /v SetupType /f
